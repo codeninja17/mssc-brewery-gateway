1faf3db6e6932778d53e45d3bdf3ae2c6ff7d5a5
codeninja17 <ankitkr09@gmail.com>
2021-06-15 19:59:41 +0530

    Spring cloud

---
+++ src/main/java/com/brewery/gateway/config/GoogleRouteConfig.java
@@ -0,0 +1,21 @@
+package com.brewery.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+public class GoogleRouteConfig {
+
+   // @Bean
+    public RouteLocator googleRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/googlesearch2")
+                        .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com")
+                )
+                .build();
+    }
+
+}
---
+++ src/main/java/com/brewery/gateway/config/LocalHostConfig.java
@@ -0,0 +1,23 @@
+package com.brewery.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostConfig {
+
+    @Bean
+    public RouteLocator googleRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("beer-service","/api/v1/**")
+                        .uri("http://localhost:8080"))
+                .route(r -> r.path("beer-order-service","/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("beer-inventory-service","/api/v1/*/inventory")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+
+}
